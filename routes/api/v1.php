9eb41774dff15eb2498946cffd7d33553d9907cf
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2026-01-02 09:57:15 +0800

    conf: folder structure & app versioning

---
+++ routes/api/v1.php
@@ -0,0 +1,10 @@
+<?php
+
+    use Illuminate\Support\Facades\Route;
+
+    Route::group([], function () {
+        require __DIR__.'/v1/auth.php';
+//        require __DIR__.'/v1/purchase.php';
+//        require __DIR__.'/v1/stock.php';
+//        require __DIR__.'/v1/master.php';
+    });
---
+++ routes/api/v1/auth.php
@@ -0,0 +1,8 @@
+<?php
+    use Illuminate\Support\Facades\Route;
+
+    Route::prefix('auth')->group(function(){
+       Route::get('/', function () {
+            return response()->json(['message' => 'Hello World']);
+       });
+    });
